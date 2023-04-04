59fb6e88338efc06ed34b026d4b70af5b2400f8c
Martin Malotky <mmalotky@dev-10.com>
2023-04-04 10:11:31 -0500

    +last

--- src/components/PauseMenu.js
+++ src/components/PauseMenu.js
@@ -1,6 +1,8 @@
+import { fadeOut } from "../animations/ComponentAnimations";
 import "./Menu.css"
+import StartMenu from "./StartMenu";
 
-function PauseMenu({unpause}) {
+function PauseMenu({unpause, setScreen}) {
     const resume = function(evt) {
         evt.preventDefault();
         unpause();
@@ -8,13 +10,15 @@ function PauseMenu({unpause}) {
 
     const exit = function(evt) {
         evt.preventDefault();
+        fadeOut(".play-screen")
+        setTimeout(() => setScreen(<StartMenu setScreen={setScreen}/>), 1000);
     }
 
     return (
         <div className="menu-container">
             <form>
                 <button onClick={(evt) => resume(evt)} className="menu-button">resume</button>
-                <button onClick={(evt) => exit(evt)} className="menu-button" disabled>exit</button>
+                <button onClick={(evt) => exit(evt)} className="menu-button">exit</button>
             </form>
         </div>
     );
