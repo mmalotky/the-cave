bae59ecb8ca0ee078d414cf4d8ff960de3da1491
Martin Malotky <mmalotky@dev-10.com>
2023-04-04 11:37:11 -0500

    first pass at movement

--- src/animations/ComponentAnimations.js
+++ src/animations/ComponentAnimations.js
@@ -1,5 +1,7 @@
 import { gsap } from "gsap";
 
+const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
 export function fadeOut(el) {
     gsap.to(el, {
         opacity:0,
@@ -10,6 +12,20 @@ export function fadeOut(el) {
 export function fadeIn(el) {
     gsap.to(el, {
         opacity:1,
+        duration:1
+    })
+}
+
+export function verticalMove(el, y) {
+    gsap.to(el, {
+        top: y * rem,
+        duration:1
+    })
+}
+
+export function horizontalMove(el, x) {
+    gsap.to(el, {
+        left: x * rem,
         duration:1
     })
 }
--- src/components/PlayScreen.js
+++ src/components/PlayScreen.js
@@ -10,6 +10,12 @@ function PlayScreen({setScreen}) {
         <PlayerAvatar key="playerAvatar"/>
     ]);
 
+    const [xMove, setXMove] = useState(false);
+    const [yMove, setYMove] = useState(false);
+
+    const [y, setY] = useState(0);
+    const [x, setX] = useState(0);
+
     const keylogger = useKeylogger();
 
     useEffect(() => {
@@ -17,8 +23,58 @@ function PlayScreen({setScreen}) {
             if(keylogger.includes('Escape')) {
                 pause();
             }
+
+            if((keylogger.includes('w') || keylogger.includes('s'))
+                    && !(keylogger.includes('w') && keylogger.includes('s'))) {
+                setYMove(true);
+            }
+            else {
+                setYMove(false);
+            }
+
+            if((keylogger.includes('a') || keylogger.includes('d'))
+                    && !(keylogger.includes('a') && keylogger.includes('d'))) {
+                setXMove(true);
+            }
+            else {
+                setXMove(false);
+            }
         }
     }, [keylogger]);
+
+    const moveY = function() {
+        if(yMove) {
+            if(keylogger.includes('w')) {
+                setY(y - 1);
+            }
+            if(keylogger.includes('s')) {
+                setY(y + 1);
+            }
+        }
+    }
+
+    useEffect(moveY, [yMove]);
+    
+    useEffect(() => {
+        console.log(y);
+    }, [y]);
+
+    const moveX = function() {
+        if(xMove) {
+            if(keylogger.includes('a')) {
+                setX(x - 1);
+            }
+            if(keylogger.includes('d')) {
+                setX(x + 1);
+            }
+        }
+    }
+
+    useEffect(moveX, [xMove]);
+
+    useEffect(() => {
+        console.log(x);
+    }, [x]);
 
     useEffect(() => {
         fadeIn(".play-screen");
